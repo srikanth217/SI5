d1a7f81c74176639e2a2b148ae8918729cb7a03a
Sai Charan <charan0017@gmail.com>
2018-11-28 13:50:45 +0530

    Updated Login code logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import Login from './Login';
-import './App.css';
 import AddEmployee from './containers/AddEmployee/AddEmployee';
 
 
 class App extends Component {
   render() {
     return (
-        <div className="root__container">
-
-         <Login/>
-
-        </div>
-
-
+        <Login/>
     );
   }
 }
--- src/Login.js
+++ src/Login.js
@@ -1,77 +1,66 @@
 import React from 'react';
 import axios from 'axios';
 import './Login.css';
-import Img from './assets/images/img1.png';
-import Header from "./Header";
 
 class Login extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
             inputFields: {
                 username: null,
                 password: null
-            }
+            },
+            isHrLogin: false,
         };
     };
+
     submitHandler = () => {
-        axios.post('/hr/login', this.state.inputFields).then(
-            () => {
-
-            }
-        );
-        console.log(this.state.inputFields);
-
+        console.log(this.state);
+        if (this.state.isHrLogin) {
+        } else {
+        }
     };
+
     userHandler = (event) => {
         let inputFields = {...this.state.inputFields};
         inputFields[event.target.name] = event.target.value;
         this.setState({
             inputFields
         });
-
-
     };
 
+    toggleIsHrLogin = () => {
+        this.setState((prevState) => {
+            return { isHrLogin: !prevState.isHrLogin };
+        });
+    };
 
     render() {
         return (
-
-            <div className="box-container">
-                <img src={Img} alt="bg" className="bg"/>
-            <div className="inner-container">
-
-                <div className="box">
-
-                    <div className="input-group">
-                        <label className="login-label" htmlFor="username">Username</label>
-                        <input
-                            type="email"
-                            name="username"
-                            className="login-input"
-                            placeholder="Username"
-                            value={this.state.username}
-                             onChange={this.userHandler}/>
+            <div className="login">
+                <div className="container justify-content-center" style={{height: '100vh'}}>
+                    <h1 className="mt-5 display-4 text-center">Log In</h1>
+                    <div className="form-group text-center">
+                        <input type="checkbox" className="switch_1" onChange={this.toggleIsHrLogin} checked={this.state.isHrLogin} />
+                        <small className="form-text text-muted">You are Logging as {!this.state.isHrLogin ? 'Employee' : 'HR'}. Toggle above to change</small>
                     </div>
-
-                    <div className="input-group">
-                        <label className="login-label" htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            name="password"
-                            className="login-input"
-                            placeholder="Password"
-                            value={this.state.password}
-                            onChange={this.userHandler}/>
+                    <div className="form">
+                        <div className="form-group">
+                            <input type="email" className="form-control form-control-lg"
+                                   placeholder="Username" name="username" onChange={this.userHandler} />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control form-control-lg"
+                                   placeholder="Password" name="password" onChange={this.userHandler} />
+                        </div>
+                        <div className="form-group">
+                            <button type="button" className="btn btn-info btn-block mt-4"
+                                    onClick={(event) =>this.submitHandler(event, this.state.inputFields)}>
+                                Login
+                            </button>
+                        </div>
                     </div>
-
-                    <button
-                        type="button"
-                        className="login-btn"
-                        onClick={(event) =>this.submitHandler(event, this.state.inputFields)}>Login</button>
                 </div>
-            </div>
             </div>
         );
     }
