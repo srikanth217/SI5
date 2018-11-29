2ac7feaaef966a3be936d7a8930b01c333d2b093
srikanth varma <srikanthvarma@srikanths-MacBook.local>
2018-11-29 03:09:04 -0500

    changes in add employee

--- src/App.js
+++ src/App.js
@@ -7,7 +7,27 @@ class App extends Component {
   render() {
     const employee = {
       firstName: 'F',
-      dateOfBirth: '12/12/2018'.split('/').reverse().join('-')
+      dateOfBirth: '12/12/2018'.split('/').reverse().join('-'),
+        lastName: 'k',
+        age:'22',
+        maritalStatus:'single',
+        jobId:'12',
+        hiringDate: '12/12/2018'.split('/').reverse().join('-'),
+        joiningDate: '12/12/2018'.split('/').reverse().join('-'),
+        nameOfSchool: 'uncc',
+        degree: 'masters',
+        startDate: '12/12/2018'.split('/').reverse().join('-'),
+        endDate: '12/12/2018'.split('/').reverse().join('-'),
+        salary: '60000',
+        isMonthly: 'false',
+        phoneNumber: '7049499235',
+        email: 'abc',
+        address: "430 barton creek drive"
+
+
+
+
+
     };
     return (
         <AddEmploye employee={employee} />
--- src/components/AddEmployee/AddEmployee.js
+++ src/components/AddEmployee/AddEmployee.js
@@ -12,65 +12,65 @@ const AddEmployee = (props) => {
                 </div>
                 <div className="form-group">
                     <input type="text" className="form-control"
-                           placeholder="Last Name" name="lastName" />
+                           placeholder="Last Name" name="lastName" value={employee && employee.lastName ? employee.lastName : null} />
                 </div>
                 <div className="form-group">
                     <input type="number" className="form-control" placeholder="Age"
-                           name="age"/>
+                           name="age"  value={employee && employee.age ? employee.age : null}/>
                 </div>
                 <div className="form-group">
                     <label className="h6 text-capitalize">date of birth</label>
                     <input type="date" className="form-control" name="dateOfBirth" value={employee && employee.dateOfBirth ? employee.dateOfBirth : null} />
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Marital Status" name="maritalStatus"/>
+                    <input type="text" className="form-control" placeholder="Marital Status" name="maritalStatus"  value={employee && employee.maritalStatus ? employee.maritalStatus : null}/>
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Gender" name="gender"/>
+                    <input type="text" className="form-control" placeholder="Gender" name="gender" value={employee && employee.gender ? employee.gender : null}/>
                 </div>
                 <div className="form-group">
                     <label className="h6 text-capitalize">hiring date</label>
-                    <input type="date" className="form-control" name="hiringDate"/>
+                    <input type="date" className="form-control" name="hiringDate" value={employee && employee.hiringDate ? employee.hiringDate : null}/>
                 </div>
                 <div className="form-group">
                     <label className="h6 text-capitalize">joining date</label>
-                    <input type="date" className="form-control" name="joiningDate" id="joiningDate"/>
+                    <input type="date" className="form-control" name="joiningDate" id="joiningDate" value={employee && employee.joiningDate ? employee.joiningDate : null}/>
                 </div>
                 <div className="form-group">
                     <input type="number" className="form-control" placeholder="Job Id"
-                           name="jobId"/>
+                           name="jobId" value={employee && employee.jobId ? employee.jobId : null}/>
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Name of the School" name="nameOfSchool"/>
+                    <input type="text" className="form-control" placeholder="Name of the School" name="nameOfSchool" value={employee && employee.nameOfSchool ? employee.nameOfSchool : null}/>
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Degree" name="degree"/>
+                    <input type="text" className="form-control" placeholder="Degree" name="degree" value={employee && employee.degree ? employee.degree : null} />
                 </div>
                 <div className="form-group">
                     <label className="h6 text-capitalize">start date</label>
-                    <input type="date" className="form-control" name="startDate"/>
+                    <input type="date" className="form-control" name="startDate" value={employee && employee.startDate ? employee.startDate : null}/>
                 </div>
                 <div className="form-group">
                     <label className="h6 text-capitalize">end date</label>
-                    <input type="date" className="form-control" name="endDate"/>
+                    <input type="date" className="form-control" name="endDate" value={employee && employee.endDate ? employee.endDate : null}/>
                 </div>
                 <div className="form-group">
                     <input type="number" className="form-control" placeholder="Salary"
-                           name="amount"/>
+                           name="amount" value={employee && employee.salary ? employee.salary : null}/>
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Is monthly" name="isMonthly"/>
+                    <input type="text" className="form-control" placeholder="Is monthly" name="isMonthly" value={employee && employee.isMonthly ? employee.isMonthly : null}/>
                 </div>
                 <div className="form-group">
                     <input type="tel" className="form-control"  placeholder="Contact Number" name="phoneNumber"
-                           pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
+                           pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={employee && employee.phoneNumber ? employee.phoneNumber : null}/>
                 </div>
                 <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Email" name="email"/>
+                    <input type="text" className="form-control" placeholder="Email" name="email" value={employee && employee.email ? employee.email : null}/>
                 </div>
                 <div className="form-group">
                     <textarea className="form-control" placeholder="Address"
-                              name="address" />
+                              name="address" value={employee && employee.address ? employee.address : null}/>
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4"/>
             </form>
